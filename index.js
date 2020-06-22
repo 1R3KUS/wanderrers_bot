d2bdd714ee293c74aac382d719387585678574b4
1R3KUS <r3kus05@gmail.com>
2020-06-22 10:38:49 +0300

    Изменил Hello на /start

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const token = '720980253:AAHSaRNaeNdO2rOMcr-sYwfVaI92R5ln6Lo';
 
 const bot = new TelegramBot(token, { polling: true });
 
+bot.on("message", msg => {
+  bot.sendMessage(msg.chat.id, 'I\'m bot Wanderrers. How can i help you?');
+})
 
-
-
-bot.onText(/Hello/, msg => {
+bot.onText(/\/start/, msg => {
   bot.sendMessage(msg.chat.id, 'I\'m bot Wanderrers. How can i help you?');
 })
 
